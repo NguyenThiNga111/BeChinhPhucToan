afa1a0b9b0eec478c454a385ae7d1711c8186bb0
Huynh Pham Tam Nguyen <tamnguyen.lisa@gmail.com>
2024-12-18 18:17:35 +0700

    Add "role" attribute into User table

--- Models/User.cs
+++ Models/User.cs
@@ -7,9 +7,10 @@ namespace BeChinhPhucToan_BE.Models
         [Key]
         public string phoneNumber { get; set; }
         public string? password { get; set; }
+        public string? role { get; set; }
         public bool? isVerify { get; set; }
         public string? otpCode { get; set; }
-        public string? otpExpiration { get; set; }
+        public string? otpExpiration { get; set; }       
 
         public Administrator? Administrator { get; set; }
         public Parent? Parent { get; set; }
