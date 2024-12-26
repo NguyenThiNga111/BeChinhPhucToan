d211d6afdeccb46c9cacd0018b54bbfbdd597c37
Huynh Pham Tam Nguyen <tamnguyen.lisa@gmail.com>
2024-12-26 23:34:31 +0700

    [CRUD] Exercise

---
+++ Controllers/ExerciseController.cs
@@ -0,0 +1,89 @@
+﻿using BeChinhPhucToan_BE.Data;
+using BeChinhPhucToan_BE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeChinhPhucToan_BE.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ExerciseController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Exercise>>> getAllExercises()
+        {
+            var exercises = await _context.Exercises.ToListAsync();
+            return Ok(exercises);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exercise>> getExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise is null)
+                return NotFound(new { message = "Exercise is not found!" });
+            return Ok(exercise);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Exercise>> addExercise([FromBody] Exercise exercise)
+        {
+            try
+            {
+                _context.Exercises.Add(exercise);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Created successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred." });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Exercise>> updateExercise([FromBody] Exercise newInfo)
+        {
+            try
+            {
+                var exercise = await _context.Exercises.FindAsync(newInfo.id);
+                if (exercise is null)
+                    return NotFound(new { message = "Exercise is not found!" });
+
+                exercise.question = newInfo.question;
+                exercise.answer = newInfo.answer;
+                exercise.lessonID = newInfo.lessonID;
+
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Updated successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred." });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> deleteExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+
+            if (exercise is null)
+                return NotFound(new { message = "Exercise is not found!" });
+
+            _context.Exercises.Remove(exercise);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Deleted successfully!" });
+        }
+    }
+}
---
+++ Controllers/LessonController.cs
@@ -0,0 +1,88 @@
+﻿using BeChinhPhucToan_BE.Data;
+using BeChinhPhucToan_BE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeChinhPhucToan_BE.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LessonController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LessonController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Lesson>>> getAllLessons()
+        {
+            var lessons = await _context.Lessons.ToListAsync();
+            return Ok(lessons);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lesson>> getLesson(int id)
+        {
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson is null)
+                return NotFound(new { message = "Lesson is not found!" });
+            return Ok(lesson);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Lesson>> addLesson([FromBody] Lesson lesson)
+        {
+            try
+            {
+                _context.Lessons.Add(lesson);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Created successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred." });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Lesson>> updateLesson([FromBody] Lesson newInfo)
+        {
+            try
+            {
+                var lesson = await _context.Lessons.FindAsync(newInfo.id);
+                if (lesson is null)
+                    return NotFound(new { message = "Lesson is not found!" });
+
+                lesson.name = newInfo.name;
+                lesson.chapterID = newInfo.chapterID;
+
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Updated successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An unexpected error occurred." });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> deleteLesson(int id)
+        {
+            var lesson = await _context.Lessons.FindAsync(id);
+
+            if (lesson is null)
+                return NotFound(new { message = "Lesson is not found!" });
+
+            _context.Lessons.Remove(lesson);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Deleted successfully!" });
+        }
+    }
+}
